2e5adc77964fb4b9fe2b39f40b1e27a66189cbc9
--global <--global>
2026-05-23 17:10:14 +0200

    i updated the chatwrapper.jsx

--- frontend/src/components/Chatwrapper.jsx
+++ frontend/src/components/Chatwrapper.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 import { API_URL } from '../config';
+import { SOCKET_URL } from '../config';
 
 
 const ChatWrapper = ({ isDarkTheme, currentUser, setCurrentUser, onClose }) => {
